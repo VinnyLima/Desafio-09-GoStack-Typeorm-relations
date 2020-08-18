bcac2eecbded2319b7ec3b046a6a1d7009aa20e4
Vinicius Lima <paulo.vinicius.f.l@live.com>
2020-08-18 13:30:07 -0300

    Add news customers

--- src/modules/orders/infra/typeorm/entities/Order.ts
+++ src/modules/orders/infra/typeorm/entities/Order.ts
@@ -10,12 +10,16 @@ import {
 
 import Customer from '@modules/customers/infra/typeorm/entities/Customer';
 import OrdersProducts from '@modules/orders/infra/typeorm/entities/OrdersProducts';
-
+@Entity('orders')
 class Order {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @ManyToOne(() => Customer)
+  @JoinColumn({name:'customer_id'})
   customer: Customer;
 
+  
   order_products: OrdersProducts[];
 
   created_at: Date;
